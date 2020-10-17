dcfa24cb78d63f0e0ff20f21891881a90c414c00
Taras <taras.korovytskyi.itis.2020@lpnu.ua>
2020-10-17 15:41:04 +0300

    3.4

---
+++ pr3.4/pr3.4.cpp
@@ -0,0 +1,27 @@
+﻿// pr3.4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// <Коровицький Тарас >
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 13
+#include <iostream>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент
+	double y; // вхідний параметр
+	double R1; // вхідний аргумент
+	double R2; // вхідний аргумент
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	cout << "R1 = "; cin >> R1;
+	cout << "R2 = "; cin >> R2;
+	// розгалуження в повній формі
+	if ((y >= sqrt(R2*R2-x*x) && y>=0 && x<=0) ||
+		(y <= sqrt((R1-R2) * (R1-R2) - (x * x)) && y <= 0 && x >= 0))
+		cout << "yes" << endl;
+	else
+		
+		cout << "no" << endl;
+	cin.get();
+	return 0;
+}
